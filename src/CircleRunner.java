fd2bafa0c796db71d2ea3604d38a8fd9cf59ade3
Tasmin Jahan <tasminjahan805@gmail.com>
2022-09-30 08:25:16 -0400

    circle

---
+++ src/CircleRunner.java
@@ -0,0 +1,14 @@
+public class CircleRunner {
+    public static void main(String[] args) {
+        // A. create a Circle object with radius 5.0
+        // B. locate the method defined in the Circle class and write
+        Circle circ1 = new Circle (5.0);
+
+        //    a line of code to call it on your object
+        circ1.printArea();
+
+        // C. BEFORE running this code, PREDICT what gets printed = 25pi
+
+        // D. run the code and confirm your prediction! Were you correct?
+    }
+}
